c94ce041e7ca7a2eb7f7f4f942863ffd8bbb9bab
UsamaEmp508 <glowingemp508@glowingsoft.com>
2024-06-04 00:24:37 +0500

    latest code

--- Src/Client/Client.jsx
+++
@@ -1,9 +0,0 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
-const BASE_URL = 'https://api.graphql.jobs/';
-
-const client = new ApolloClient({
-  uri: BASE_URL,
-  cache: new InMemoryCache(),
-});
-
-export default client;
--- Src/Service/graphql.js
+++
@@ -1,9 +0,0 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
-
-export const apolloClient = new ApolloClient({
-  uri: `https://api.hireacar.pk/graphql` ,
-  cache: new InMemoryCache(),
-}); 
-
-
-  // uri: `https://api.hireacar.pk/graphql` as any,
